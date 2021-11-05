8fadb385c45d40b563d6cf9077148fffda2ae7e6
Alex Bertotto <ajb5206@gmail.com>
2021-11-05 13:35:19 -0700

    add flvaor and treat models

---
+++ PierreTreats/Models/Flavor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace PierreTreats.Models
+{
+	public class Flavor
+	{
+		public Flavor()
+		{
+			this.JoinEntities = new HashSet<FlavorTreat>();
+		}
+
+		public int FlavorId { get; set; }
+		public string FlavorName { get; set; }
+		public string FlavorDescription { get; set; }
+
+		public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+	}
+}
