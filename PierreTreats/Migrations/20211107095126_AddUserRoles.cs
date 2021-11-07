4aed80133c445f2a81ae1b696b8a6712d3827629
Alex Bertotto <ajb5206@gmail.com>
2021-11-07 01:52:26 -0800

    add user and owner roles within AddUserRoles migration

---
+++ PierreTreats/Migrations/20211107095126_AddUserRoles.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierreTreats.Migrations
+{
+    public partial class AddUserRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) Values (1,'Owner','OWNER')");
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) Values (2, 'Patron', 'PATRON')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = 1");
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = 2");
+        }
+    }
+}
